ed0909536041710c3677c12e74df12abd3502346
jpoppler <jason@getnotion.com>
2020-04-11 16:19:50 -0600

    analyzer updates
    * include season name along with agent name
    * plot those names in the title
    * shift y min to 0

--- lib/analyzer.py
+++ lib/analyzer.py
@@ -1,14 +1,16 @@
+import pdb
 import os
-import matplotlib.pyplot as plt
 import json
+import matplotlib.pyplot as plt
 
 class Analyzer(object):
     RUNS_DIR = 'runs'
     os.makedirs(RUNS_DIR, exist_ok=True)
 
-    def __init__(self, name):
+    def __init__(self, name, season):
         self.name     = name
-        self.cwd      = Analyzer.RUNS_DIR + '/' + name
+        self.season   = season
+        self.cwd      = Analyzer.RUNS_DIR + '/' + name + '/' + season
         self.rewards  = []
         self.episodes = []
         self.moves    = []
@@ -22,8 +24,13 @@ class Analyzer(object):
         self.rewards_vs_episodes()
 
     def save_digest(self, digest):
+        digest = {
+            "name": self.name,
+            "season": self.season,
+            "facts": digest.facts,
+        }
         with open(self.cwd + '/' + 'digest.json', 'w') as fp:
-            json.dump(digest.facts, fp)
+            json.dump(digest, fp, indent=1)
 
     def unpack_digest(self, digest):
         for fact in digest.facts:
@@ -32,16 +39,22 @@ class Analyzer(object):
             self.moves.append(fact["moves"])
 
     def rewards_vs_episodes(self):
+        plt.figure()
         self.plot_y_vs_x('rewards', self.rewards, 'episodes', self.episodes)
+        plt.draw()
         plt.savefig(self.cwd + '/' + 'rewards_vs_episodes')
 
     def moves_vs_episodes(self):
+        plt.figure()
         self.plot_y_vs_x('moves', self.moves, 'episodes', self.episodes)
+        plt.draw()
         plt.savefig(self.cwd + '/' + 'moves_vs_episodes')
 
     def plot_y_vs_x(self, y_label, y_data, x_label, x_data):
-        plt.title(y_label + " vs " + x_label)
+        title = "{} vs {} ({} / {})".format(y_label, x_label, self.name, self.season)
+        plt.title(title)
         plt.plot(x_data, y_data, 'bo')
+        plt.ylim(bottom=0)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
 
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from lib.game_master  import GameMaster
 from lib.analyzer     import Analyzer
 
 ATARI_GAME   = 'SpaceInvaders-v0'
-RUN_NAME     = 'random_agent'
-NUM_EPISODES = 50
+AGENT        = 'random_agent'
+SEASON       = "season_1"
+NUM_EPISODES = 5
 
 def main():
-    analyzer    = Analyzer(RUN_NAME)
+    analyzer    = Analyzer(AGENT, SEASON)
     env         = gym.make(ATARI_GAME)
     agent       = RandomAgent(env.action_space.n)
     game_master = GameMaster(env, agent)
-    digest      = game_master.run_season(RUN_NAME, NUM_EPISODES, render=False)
+    digest      = game_master.run_season(SEASON, NUM_EPISODES, render=False)
     analyzer.run(digest)
 
 main()
