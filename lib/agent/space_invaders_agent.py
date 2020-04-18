97ba290ec9acd9452d02bad6ab39a4a980708b41
jpoppler <jason@getnotion.com>
2020-04-17 21:50:19 -0600

    start impelentation on space invaders agent
    * constructor takes in:
       - alpha: learning rate
       - epsilon: expoloration parameter
       - gamma: discount factor
       - memory_size/recall_size: parameters for Memory
    
    * select_action
      - this will decide if it should choose a random action or take a
      greedy action from the Q network (Q network not implemented yet)

--- lib/agent/space_invaders_agent.py
+++ lib/agent/space_invaders_agent.py
@@ -1,16 +1,25 @@
 import random
 import numpy as np
 from .agent import Agent
+from .memory import Memory
 
 class SpaceInvadersAgent(Agent):
-    def save(self):
-        return
+    def __init__(self, max_action, alpha, epsilon, gamma, memory_size=50000, recall_size=32):
+        super().__init__(max_action)
 
-    def load(self, training):
-        return
+        self.alpha   = alpha
+        self.epsilon = epsilon
+        self.gamma   = gamma
+        self.memory  = Memory(memory_size, recall_size)
 
     def select_action(self, observation):
-        return random.choice(self.valid_actions)
+        action = None
+        if random.uniform(0,1) < self.epsilon:
+            action = random.choice(self.valid_actions)
+        else:
+            # get action from neural network
+            action = None
+        return action
 
     def train(self, env, episodes):
         return
--- main.py
+++ main.py
@@ -8,20 +8,23 @@ from lib.game_master                import GameMaster
 from lib.analyzer                   import Analyzer
 from lib.command_line_parser        import cmd_parse
 
-ATARI_GAME = 'SpaceInvaders-v0'
+ATARI_GAME  = 'SpaceInvaders-v0'
+ALPHA       = 0.001
+EPSILON     = 1.0
+GAMMA       = 0.95
 
 def main(argv):
     (command, episodes, season) = cmd_parse(argv)
 
     file_manager = FileManager(ATARI_GAME, season)
     env          = gym.make(ATARI_GAME)
-    agent        = SpaceInvadersAgent(env.action_space.n)
+    agent        = SpaceInvadersAgent(env.action_space.n, ALPHA, EPSILON, GAMMA):
     game_master  = GameMaster(env, agent)
     analyzer     = Analyzer(file_manager.cwd)
 
     if command == 'train':
         agent.train(env, episodes)
-        file_manager.save(agent.save(), 'training.json')
+        #file_manager.save(agent.save(), 'training.json')
     elif command == 'analyze':
         agent.load(file_manager.load('training.json'))
         digest = game_master.run_season(episodes)
